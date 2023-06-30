13d1ef32fea4cf737c6508eceb7e49ea5fcf6465
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2023-06-30 08:59:21 +0300

    two_commit

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,35 @@
-import express, {Request, Response} from 'express' 
+import express, { Request, Response } from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 
-app.get('/', (req: Request, res: Response) => {
-   res.send('Stroy-city')
+app.get('/about', (req: Request, res: Response) => {
+   res.end(`<div>
+         <h1>Stroy-city</h1> 
+         <p>Home page</p> 
+         <nav>
+            <ul> 
+               <li> <a href="/about">Home page</a> </li>
+               <li> <a href="/services">Services page</a> </li>
+            </ul>
+         </nav>
+            
+      </div>`)
+})
+
+app.get('/services', (req: Request, res: Response) => {
+   res.end(`<div> 
+   <h2>Stroy-city</h2> 
+   <p>Services page</p>  
+   <nav>
+   <ul> 
+      <li> <a href="/about">Home page</a> </li>
+      <li> <a href="/services">Services page</a> </li>
+   </ul>
+</nav> </div>`)
 })
 
 app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
+   console.log(`Example app listening on port ${ port }`)
 })
